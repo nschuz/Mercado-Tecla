33da4dff723c98a86a916d8f87f41ad55d2c0b85
Chuz <jjregis.valero@gmail.com>
2021-06-26 13:17:06 -0500

    listo

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,16 @@
 const selector = document.querySelector('#sel1');
+const boton = document.querySelector('#boton')
 
+document.addEventListener('DOMContentLoaded', () => {
+    boton.addEventListener('click', buscarProducto);
 
-//document.addEventListener('DOMContentLoaded', () => {
-// formulario.addEventListener('submit', validaBusqueda);
-//});
+});
 
 
 
 function consultarAPI() {
 
-    const url = `https://api.mercadolibre.com/sites/MLA/categories`
+    const url = `https://api.mercadolibre.com/sites/MLM/categories`
     fetch(url)
         .then((res) => res.json())
         .then((data) => {
@@ -29,11 +30,42 @@ function recorrerJson(data) {
 }
 
 function insertarOption(name, id) {
-    console.log(name);
+    //console.log(name);
     const datoSelect = document.createElement('option')
     datoSelect.innerHTML = name
+    datoSelect.value = id
     const selector = document.querySelector('#sel1');
     selector.appendChild(datoSelect)
+}
 
-    // getValor()
+
+function buscarProducto(e) {
+    e.preventDefault();
+    let sel = document.getElementById("sel1");
+    let valor = sel.value;
+    consultarProductos(valor);
+
+}
+
+function consultarProductos(valor) {
+    const url = `https://api.mercadolibre.com/sites/MLM/search?category=${valor}`
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            insertarData(data)
+        })
+        .catch((err) => console.log(err));
+
+}
+
+function insertarData(data) {
+    const { results } = data
+    console.log(results);
+    for (let i = 0; i < 8; i++) {
+        const { title, price, thumbnail } = results[i]
+        console.log(title);
+        console.log(price)
+        console.log(thumbnail);
+    }
+
 }
