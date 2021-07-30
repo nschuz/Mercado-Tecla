74b40f0f269d4ada9cba6884a3cbada1605148b8
Chuz <jjregis.valero@gmail.com>
2021-07-30 11:54:57 -0500

    Fixed Heroku test 1

--- backend/db/conexion.js
+++ backend/db/conexion.js
@@ -5,23 +5,38 @@ let sequelize;
 
 
 try {
-    sequelize = new Sequelize(process.env.DBAZURE, process.env.USERAZURE, process.env.PASSAZURE, {
+    // sequelize = new Sequelize(process.env.DBAZURE, process.env.USERAZURE, process.env.PASSAZURE, {
+    //     //host: process.env.HOST,
+    //     host: process.env.HOSTAZURE,
+    //     dialect: process.env.dialect,
+    //     pool: {
+    //         max: 5,
+    //         min: 0,
+    //         idle: 10000
+    //     },
+    //     dialectOptions: {
+    //         encrypt: true
+    //     }
+    // });
+
+    sequelize = new Sequelize('tecla', 'teclaadmin', 'root123$', {
         //host: process.env.HOST,
-        host: process.env.HOSTAZURE,
-        dialect: process.env.dialect,
+        host: 'tecla.database.windows.net',
+        dialect: 'mssql',
         pool: {
             max: 5,
             min: 0,
             idle: 10000
+
         },
         dialectOptions: {
             encrypt: true
         }
     });
+
     console.log("Conexion a la base de datos correcta".green);
 } catch (e) {
     console.log("Problema en la conexion ".red + e);
-
 }
 
 
